9c2f8f8beb8d51de0a410af5d7888c86e34368ef
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-07-02 14:22:33 +0200

    Add files for TP1 and 2

---
+++ src/main/java/TestJpa/TestJpa.java
@@ -0,0 +1,84 @@
+package TestJpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import TestJpa.entites.Livre;
+
+public class TestJpa {
+
+	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		System.out.println(entityManager);
+
+		// selection d'un livre selon l'ID et affichage du titre et de l'auteur
+		Livre l = entityManager.find(Livre.class, 1);
+		System.out.println("Titre : " + l.getTitre() + ", Auteur : " + l.getAuteur());
+
+		// insertion d'un nouveau livre dans la BDD : necessite un contexte de
+		// transaction
+		EntityTransaction tx = entityManager.getTransaction();
+		tx.begin();
+
+		Livre newLivre = new Livre(6, "Biochimie", "Donald VOET et Judith G.VOET");
+		try {
+			entityManager.persist(newLivre);
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		// modification du livre avec l'id 5
+
+		Livre livreAmodifier = entityManager.find(Livre.class, 5);
+		if (livreAmodifier != null) {
+			livreAmodifier.setTitre("Du plaisir dans la cuisine");
+		}
+		try {
+			entityManager.merge(livreAmodifier);
+			tx.commit();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		// requête JPQL sur le titre du livre
+		String titreARecuperer = "Biochimie";
+		TypedQuery<Livre> query = entityManager
+				.createQuery("select l from Livre l where titre='" + titreARecuperer + "'", Livre.class);
+		List<Livre> listeLivres = query.getResultList();
+		System.out.println(listeLivres.get(0));
+
+		// requête JPQL sur l'auteur
+		String auteurARecuperer = "Jules Verne";
+		TypedQuery<Livre> query2 = entityManager
+				.createQuery("select l from Livre l where auteur='" + auteurARecuperer + "'", Livre.class);
+		List<Livre> listeLivres2 = query2.getResultList();
+		System.out.println(listeLivres2.get(0));
+
+		// suppression d'un livre
+		Livre livreASupprimer = entityManager.find(Livre.class, 2);
+		if (livreASupprimer != null) {
+			System.out.println("Suppression du livre : " + livreASupprimer);
+			entityManager.remove(livreASupprimer);
+			tx.commit();
+		}
+
+		// affichage de tous les livres dans la BDD
+		TypedQuery<Livre> query3 = entityManager.createQuery("select l from Livre l", Livre.class);
+		List<Livre> listetousLivres = query3.getResultList();
+		for (Livre livre : listetousLivres) {
+			System.out.println("Titre : " + livre.getTitre() + "\t Auteur : " + livre.getAuteur());
+		}
+
+		entityManager.close();
+		entityManagerFactory.close();
+	}
+
+}
---
+++ src/main/java/TestJpa/entites/Livre.java
@@ -0,0 +1,102 @@
+package TestJpa.entites;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LIVRE")
+public class Livre {
+	@Id
+	@Column(name="ID")
+	private Integer id;
+	@Column(name = "TITRE")
+	private String titre;
+	@Column(name = "AUTEUR")
+	private String auteur;
+
+	/**
+	 * Getter
+	 * 
+	 * @return the titre
+	 */
+	public String getTitre() {
+		return titre;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param titre the titre to set
+	 */
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the auteur
+	 */
+	public String getAuteur() {
+		return auteur;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param auteur the auteur to set
+	 */
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	/**
+	 * Getter
+	 * 
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * Setter
+	 * 
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * Constructeur
+	 * 
+	 * @param id
+	 * @param titre
+	 * @param auteur
+	 */
+	public Livre(Integer id, String titre, String auteur) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.auteur = auteur;
+	}
+
+	/**
+	 * Constructeur
+	 * 
+	 */
+	public Livre() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "Livre [id=" + id + ", titre=" + titre + ", auteur=" + auteur + "]";
+	}
+
+	
+
+}
